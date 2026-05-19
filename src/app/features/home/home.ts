990442fd40882d75827339602c1b5a1331c3dff4
Alejo-Chavez <aledchav@example.com>
2026-05-19 11:22:30 -0300

    REFACTOR: haciendo escalable logica de los juegos en home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Game } from './game-card-info/game-card-info.model';
+import { GAMES } from './data/games.data';
 
 @Component({
   selector: 'app-home',
@@ -8,28 +10,6 @@ import { RouterModule } from '@angular/router';
 })
 
 export class Home {
-  games = [{
-    title: 'Ahorcado',
-    description: 'El clasico y divertido juego de ahorcado',
-    route: '/games/ahorcado',
-    icon: 'fa-gamepad'
-  },
-  {
-    title: 'Mayor o Menor?',
-    description: 'Tendrás la suerte necesaria para adivinar si tu siguiente será mayor o menor?',
-    route: '/games/mayor-o-menor',
-    icon: 'fa-chess'
-},
-{
-    title: 'Juego 3',
-    description: 'Descripción del juego',
-    route: '/mayor-o-menor',
-    icon: 'fa-chess'
-},
-{
-    title: 'Juego 4',
-    description: 'Descripción del juego',
-    route: '/mayor-o-menor',
-    icon: 'fa-chess'
-}]
-}
+  games: Game[] = GAMES
+} 
+  
